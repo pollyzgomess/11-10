bbaaaee4f724a6073d7fac8037607897cade9a69
pollyzgomess <164032722+pollyzgomess@users.noreply.github.com>
2024-10-11 14:53:34 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'programação',
+     'O que é javascript?',
+     'O javascript é uma linguagem de programação'
+)
+criaCartao(
+    'geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em inglês se diz HI (RAI)'
+)
